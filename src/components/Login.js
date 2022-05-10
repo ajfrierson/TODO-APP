2a5a68a9e5c04ebe9f63f23345f30e25f23ab449
alvin frieson <alvinfrierson@fuller.edu>
2022-05-09 18:51:58 -0700

    added state to handle accepting a username and password and update state.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import '../components/Login.css';
 
 function LoginComponent() {
+
+    let [password, setPassword] = useState("");
+    let [username, setUsername] = useState("");
+
+    function handleUsernameChange(e){
+        setUsername(e.target.value);
+      
+    }
+
+    function handlePasswordChange(e){
+        setPassword(e.target.value);
+    
+    }
+
     return(
         <div className="LoginComponent">
-            Username: <input type="text" name="username"/>
-            Password: <input type="password" name="password"/>
+            Username: <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
+            Password: <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
 
             <button className="loginButton">Login</button>
         </div>
