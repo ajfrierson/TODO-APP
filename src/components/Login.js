fe5c35b123ecf88a4e24b0f33a99f99100d14286
alvin frieson <alvinfrierson@fuller.edu>
2022-05-09 19:35:12 -0700

    hard coded login validation to the login page.

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,10 @@ import '../components/Login.css';
 
 function LoginComponent() {
 
-    let [password, setPassword] = useState("");
-    let [username, setUsername] = useState("");
+    let [password, setPassword] = useState('');
+    let [username, setUsername] = useState('');
+
+    let [loggedIn, setLoggedIn] = useState(false);
 
     function handleUsernameChange(e){
         setUsername(e.target.value);
@@ -16,14 +18,34 @@ function LoginComponent() {
     
     }
 
+    function loginClick(){
+        if(username == 'username' && password == "password"){
+            setLoggedIn(true);
+
+        } else {
+            setLoggedIn(false);
+        }
+    }
+
+    function ShowValidCredentials(){
+        if(loggedIn){
+            return <div>Login Successful!!</div>
+        } else {
+            return <div>Invalid Credentials!!</div>
+        }
+    }
+
     return(
         <div className="LoginComponent">
+            <ShowValidCredentials loggedIn={setLoggedIn}/>
             Username: <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
             Password: <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
 
-            <button className="loginButton">Login</button>
+            <button className="loginButton" onClick={loginClick}>Login</button>
         </div>
     );
 }
+
+
 
 export default LoginComponent;
